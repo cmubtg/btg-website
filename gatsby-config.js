14978fa6c7abd38035bad6fd1e32395d8a48d74b
David You <david.you@beacon.io>
2022-01-04 18:44:17 -0500

    Added projects to config.yml file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "CMUBTG Website",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "This is the website for Carnegie Mellon University Business Technology Group."
   },
   plugins: [
     "gatsby-plugin-react-helmet",
