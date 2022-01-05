05d75dac1137aa951cc762f1f479ecc165113c30
David You <david.you@beacon.io>
2022-01-04 21:14:26 -0500

    Added graphQl components to individual member page. Added sample information to test functionality.

--- src/pages/members/index.js
+++ src/pages/members/index.js
@@ -1,16 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Layout from "../../components/Layout";
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
 
 
-class Temp extends Component {
+class MemberListTemplate extends React.Component {
   render() {
+    const { data } = this.props
+    const { edges: members } = data.allMarkdownRemark
+
     return (
       <Layout>
-        <h1>Hello world!</h1>
+        {members.map(({ node: member }) => (
+          <p>
+            <Link
+              className="title has-text-primary is-size-4"
+              to={member.fields.slug}
+            >
+              {member.frontmatter.name}
+            </Link>            
+          </p>
+
+        ))}
       </Layout>
       
     );
   }
 }
 
-export default Temp;
+MemberList.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export default function MemberList() {
+  return (
+    <StaticQuery
+      query={graphql`
+        query MemberListQuery {
+          allMarkdownRemark(
+            sort: {order: DESC, fields: [frontmatter___date]}
+            filter: {frontmatter: {templateKey: {eq: "member"}}}
+          ) {
+            edges {
+              node {
+                id
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                  templateKey
+                  name
+                  photo {
+                    childImageSharp {
+                      gatsbyImageData(
+                        width: 100
+                        quality: 100
+                        layout: CONSTRAINED
+                      )
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }              
+      `}
+      render={(data, count) => <MemberListTemplate data={data} count={count} />}
+    />
+  )
+}
--- src/templates/member.js
+++ src/templates/member.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-import { kebabCase } from "lodash";
-import { Helmet } from "react-helmet";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
-import Content, { HTMLContent } from "../components/Content";
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 
-class Temp extends Component {
-  render() {
-    return (
-      <Layout>
-        <h1>Hello world!</h1>
-      </Layout>
-      
-    );
-  }
+const Member = ({ data }) => {
+  const { markdownRemark: member } = data;
+  const photo = getImage(member.frontmatter.photo)
+
+  return (
+    <Layout>
+      <h1>{member.frontmatter.name}</h1>
+      <h1>{member.frontmatter.andrewID}</h1>
+      <h1>{member.frontmatter.description}</h1>
+      <GatsbyImage image={photo} alt={member.frontmatter.name} />
+      <a href={member.frontmatter.resume.publicURL} target="_blank">Resume</a>          
+    </Layout>
+  )
 }
 
-export default Temp;
+Member.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+};
+
+export default Member
+
+export const pageQuery = graphql`
+  query MemberByAndrewID($id: String!) {
+    markdownRemark(id: {eq: $id}) {
+      id
+      frontmatter {
+        title
+        name
+        andrewID
+        description
+        linkedIn
+        portfolio
+        resume {
+          publicURL
+        }
+        photo {
+          childImageSharp {
+            gatsbyImageData(width: 400, quality: 100, layout: CONSTRAINED)
+          }
+        }
+      }
+    }
+  }
+`
