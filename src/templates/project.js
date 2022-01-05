143d26824f4e10d42cfefd4ab4883e3a848c24d9
David You <david.you@beacon.io>
2022-01-05 11:50:06 -0500

    Updated project.js to remove unimplemented portion of data

--- src/templates/project.js
+++ src/templates/project.js
@@ -6,9 +6,6 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 
 const Project = ({ data }) => {
-  const { markdownRemark: member } = data;
-  const photo = getImage(member.frontmatter.photo)
-
   return (
     <Layout>
       <h1>Hello World!</h1>
