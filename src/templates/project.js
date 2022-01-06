5e72c7dbc479cc21a03e5eb5558d449d6e257723
David You <david.you@beacon.io>
2022-01-06 14:27:51 -0500

    Updated individual project page

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import { graphql, Link } from "gatsby";
-import Layout from "../components/Layout";
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import FadeIn from "react-fade-in";
+import { Helmet } from "react-helmet";
+import BTGCover from "../images/btg-cover.png";
 
 const Project = ({ data }) => {
   return (
-    <Layout>
-      <h1>Hello World!</h1>
-    </Layout>
+<FadeIn>
+    <Helmet>
+        <title>Members | CMUBTG</title>
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:image" content={BTGCover}></meta>
+    </Helmet>      
+    <Navigation />
+
+    <Container className="mt-md-1 pt-md-4">
+      <Row>
+        <h1 className="display-3 text-black font-weight-boldest">Project Name</h1>
+      </Row>
+
+    </Container>
+
+    <Footer />
+    </FadeIn>
   )
 }
 
