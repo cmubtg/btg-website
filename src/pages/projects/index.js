372433616f37270b504b0804f7c249279d259a77
David You <david.you@beacon.io>
2022-01-05 21:11:01 -0500

    Updated alumni and all projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import Layout from "../../components/Layout";
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import FadeIn from "react-fade-in";
+import { Helmet } from "react-helmet";
+import Footer from "../../components/Footer";
+import Navigation from "../../components/Navigation";
+import BTGCover from "../../images/btg-cover.png";
 
 
 class ProjectListTemplate extends React.Component {
@@ -10,11 +17,33 @@ class ProjectListTemplate extends React.Component {
     const { edges: projects } = data.allMarkdownRemark
 
     return (
-      <Layout>
-        {projects.map(({ node: project }) => (
-          <p>{project.frontmatter.name}</p>
-        ))}
-      </Layout>
+      <FadeIn>
+        <Navigation />
+
+        <Helmet>
+          <title>Projects | CMUBTG</title>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta name="twitter:image" content={BTGCover}></meta>
+        </Helmet>
+
+        <Container className="mt-md-1 pt-md-4">
+        <Row className="pt-1 mt-5">
+          <Col>
+            <h1 className="display-3 text-black font-weight-boldest">Projects</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {projects.map(({ node: project }) => (
+              <p>{project.frontmatter.title}</p>
+            ))}          
+          </Col>
+        </Row>
+      </Container>
+
+        <Footer />
+      </FadeIn>      
+ 
       
     );
   }
