43e1c49dec9052d3efcd884a10ea45bc4075dcf2
David You <david.you@beacon.io>
2022-01-08 14:48:22 -0500

    Addded gatsby-plugin-styled-components to help with server-side rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,8 @@ module.exports = {
     },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    "gatsby-transformer-sharp",
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
