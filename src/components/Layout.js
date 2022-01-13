e1ef9d42ae23609093925820e484d6361fa94a99
Harrison Chui <hchui@macbook-air.myfiosgateway.com>
2022-01-12 21:31:16 -0500

    Resolved some warnings

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css'
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import { Helmet } from "react-helmet";
-import BTGCover from "../images/btg-cover.png";
 import FadeIn from "react-fade-in";
 
 export default function Layout({ children }) {
