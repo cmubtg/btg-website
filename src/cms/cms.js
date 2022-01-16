5fa160b3e04c7d2e7ca627048cb787ffde41bfc0
David You <dyou3968@gmail.com>
2022-01-16 10:20:41 -0500

    Uninstalled media libraries

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,7 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+// import CMS from 'netlify-cms-app'
+// import uploadcare from 'netlify-cms-media-library-uploadcare'
+// import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+// CMS.registerMediaLibrary(uploadcare)
+// CMS.registerMediaLibrary(cloudinary)
